d0499131669ad2d2873401598bb96bca8ee479d2
pgupta4 <pgupta4@stevens.edu>
2016-12-17 13:33:06 -0500

    Add files via upload
    Main methods for loop detection and removal file

---
+++ main.java
@@ -0,0 +1,10 @@
+
+public class main {
+		public static void main(String []args)
+		{
+			Loopdetection d = new Loopdetection();
+			d.createLoop();
+			d.Loopdetection();
+			//d.printlist();
+		}
+}
